e45425d3ec26df063791d826521455539a942d76
xyena <spotcheckd20@gmail.com>
2024-04-09 15:28:43 -0500

    Laying more framework

---
+++ !js/site.js
@@ -0,0 +1,5 @@
+//Head
+document.getElementById("headtitle").innerHTML="Yeenframe";
+//Footer
+document.getElementById("ftr").innerHTML="<div id='topfooter'>Site author: Hyena#759. For site-related inquiries: <a href='mailto:niceinternetjoke@gmail.com'>niceinternetjoke@gmail.com</a></div><div id='botfooter'>Digital Extremes Ltd and Warframe are registered trademarks. All rights are reserved worldwide. This site has no official link with Digital Extremes Ltd or Warframe. All artwork, screenshots, characters or other recognizable features of the intellectual property relating to these trademarks are likewise the intellectual property of Digital Extremes Ltd.</div>";
+	
